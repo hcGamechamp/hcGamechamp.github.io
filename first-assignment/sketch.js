2b975236737baf5c5ff7fa0d928ca2782b26a11d
Marquis1302272 <1302272@sps-student.sk.ca>
2022-10-04 12:13:49 -0600

    started system

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -8,6 +8,7 @@
 let cookieImage;
 let img;
 let backgroundImage;
+let score = 0;
 
 function preload() {
   cookieImage = loadImage("RealCookie.png");
@@ -16,11 +17,27 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(220);
-  image(backgroundImage, 0, 0, width, height  );
+  image(backgroundImage, 0, 0, width, height);
   image(cookieImage, 500, 150);
-  
+  mouseClicked();
+  mouseInCircle(500, 1025, 400, 700);
 }
+
+function mouseClicked() {
+  score = score + 1;
+}
+
+function mouseInCircle(left, right, top, bottom) {
+  return mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom;
+}
+
+function scoreNumber() {
+  textSize(50);
+  text("Score", 900, 150);
+}
+
